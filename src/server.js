8f56aca4144ec79302c10e3ccab36019ec84e40a
dilan-flores <117755180+dilan-flores@users.noreply.github.com>
2023-07-09 23:04:54 -0500

    swagger server.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 // Realizar importaciones
 import express from 'express'
-    // Para el manejo de variables de entorno
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 import dotenv from 'dotenv'
     // Para acceder a URL de otra página
 import cors from 'cors';
@@ -14,6 +17,22 @@ import routerPacientes from './routers/paciente_routes.js'
 const app = express()
 dotenv.config()
 
+//SwaggerUI
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
+
+const swaggerSpec={
+    definition:{
+        openapi: "3.0.0",
+        info:{
+            title: "Node MongoDB API",
+            version: "1.0.0"
+        }
+    },
+    apis: [`${__dirname}/routers/paciente_routes.js`],
+};
+
+
 // Configuraciones 
     //Utilizar el puerto 3000 o uno predeterminada al realizar el despliegue u otras
 app.set('port',process.env.port || 3000)
@@ -22,7 +41,6 @@ app.use(cors())
 // Middlewares 
     // Para trabajar con ..JSON
 app.use(express.json())
-
 
 // Variables globales
 
@@ -34,6 +52,8 @@ app.use(express.json())
 
 app.use('/api',routerVeterinarios)
 app.use('/api',routerPacientes)
+
+app.use("/api-doc",swaggerUI.serve,swaggerUI.setup(swaggerJSDoc(swaggerSpec)))
     // Manejo de una ruta que no sea encontrada
 app.use((req,res)=>res.status(404).send("Endpoint no encontrado - 404"))
 
